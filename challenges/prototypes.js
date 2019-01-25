048267ba64c3f59d980a7a8c11fc1c80fbabf370
Preprocessing - I <karenly1961@gmail.com>
2019-01-25 11:18:06 -0500

    Initial Commit

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,6 +5,11 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
+function CuboidMaker(vol) {
+  this.length = vol.length;
+  this.width = vol.length;
+  this.height = vol.height;
+}
 
 
 /* == Step 2: Volume Method ==
